df35b1519b674b8680bbb62436a98f668cd3e4cd
abhishekauti10 <abhishek.auti10@gmail.com>
2021-10-11 14:04:51 +0530

    input from number 3 in cource 1

--- Cource1.java
+++
@@ -1,3 +0,0 @@
-Abhishek G Auti
-2-Inheritence and Polymorphism 
-Inheritance supports the concept of reusability and reduces code length in object-oriented programming. Polymorphism allows the object to decide which form of the function to implement at compile-time (overloading) as well as run-time (overriding).
